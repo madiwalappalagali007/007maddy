f3b860a0737153b8f54fec64bfcef0e46d6121bf
Maddy-lagali <53235402+madiwalappalagali007@users.noreply.github.com>
2022-10-13 11:25:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php  
-   echo "Hello World";  
-?>   
+<!DOCTYPE html>
+<html>
+<head>
+<title>Embed PHP in a .html File</title>
+</head>
+<body>
+<h1><?php echo "Hello World" ?></h1>
+</body>
+</html>
